3738e5b8cef5936d2baa7b970ebe50b0bd9c752f
Lloyd Davies <39085284+Lloyd-Davies@users.noreply.github.com>
2026-03-10 20:58:40 +0000

    fix(worker): register send_email task on startup

--- worker/tasks/__init__.py
+++ worker/tasks/__init__.py
@@ -4,3 +4,4 @@ from photostore.celery_app import celery_app as app  # noqa: F401
 
 from .archive import archive_event, restore_event  # noqa: F401
 from .build_zip import build_zip  # noqa: F401
+from .send_email import send_email  # noqa: F401
